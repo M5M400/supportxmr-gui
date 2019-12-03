0b778e7b0de91b159747c1567d342dd55f5fdcf4
MoneroOcean <support@moneroocean.stream>
2019-12-02 23:49:17 -0800

    Actually minimize html

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 
 uglifycss --output build/style_min.css style.css &&\
 uglifyjs  --output build/script_min.js script.js &&\
-html-minifier --output build/index.html index.html &&\
+html-minifier --output build/index.html index.html --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true &&\
 echo OK
