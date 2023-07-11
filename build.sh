940ef5179fe3b5f5014c49e557e2a7f382d5c7b3
MoneroOcean <support@moneroocean.stream>
2023-07-11 00:40:56 +0000

    Disabled web miner

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ uglifycss --output build/style_min.css style.css &&\
 #uglifyjs  --output build/script_min.js script.js web_miner/miner.js &&\
 #uglifyjs  --output build/worker.js web_miner/worker.js &&\
 uglifyjs  --output build/script_min.js script.js &&\
-cp web_miner/cn.min.js build/cn.min.js &&\
+#cp web_miner/cn.min.js build/cn.min.js &&\
 html-minifier --output build/index-raw.html index.html --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true &&\
 node critical.js &&\
 rm build/index-raw.html &&\
